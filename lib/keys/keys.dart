7d990a593648cf33351826c196d1b035bca72538
Ashmit <ashmitsangtani3@gmail.com>
2024-11-07 01:14:08 +0530

    commit

---
+++ lib/keys/keys.dart
@@ -0,0 +1,8 @@
+class Keys {
+  final clientId =
+      "33OkryzDZsIeWm9w9_Nl44yh8HBPSasQ4h_UjP3tVzC7yZ9dPemii-ohO65DbQywbtVqyePd-JlH2WX4RMogqw==";
+  final clientSecret =
+      "lrFxI-iSEg-uI-xJKq3_HaywpIoMYZAiq6EkSqERgk2RFyjaK9CguQ0ua9bk8iBuCriFzlhwZPRw4K2tlL0xsR0Z0CTRQErm";
+  final mapSdkKey = "65edcc038e06975ce164bfa7bf68aa35";
+  final mapRestApiKey = "65edcc038e06975ce164bfa7bf68aa35";
+}
